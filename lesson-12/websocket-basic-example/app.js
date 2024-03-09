e30174f73d4a3794899700fdc371fb080d110ddf
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-03-09 09:00:48 +0100

    websocket basic example is completed

--- lesson-12/websocket-basic-example/app.js
+++ lesson-12/websocket-basic-example/app.js
@@ -2,6 +2,17 @@ const ws = new require("ws");
 
 const wsServer = new ws.Server({ port: 5000 });
 
-wsServer.on("connection", () => {
-  console.log("New frontend connected");
+const socketList = [];
+
+wsServer.on("connection", (socket) => {
+  //   console.log("New frontend connected");
+  socketList.push(socket);
+  setTimeout(() => {
+    socket.send("Welcome to web-socket server");
+  }, 3000);
+  socketList.forEach((item) => {
+    if (item !== socket) {
+      item.send("New member is connected");
+    }
+  });
 });
